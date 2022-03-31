4723bfd000f3fe9aba6c2833c6fb77aa4155434a
rupalisam1999 <rupalisangale1999@gmail.com>
2022-03-31 17:50:05 +0530

    [Rupali]ADDED: Retrieve records using group by id with count

--- Program.cs
+++ Program.cs
@@ -35,6 +35,7 @@ namespace ProductReviewManagement_Linq
             Management management = new Management();
             management.topRecords(list);
             management.selectedRecords(list);
+            management.retrieveCountOfRecords(list);
         }
        
     }
